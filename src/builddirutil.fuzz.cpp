94def331d4e1693c914772ebaf13e78d806635a2
Elliot Goodrich <elliotgoodrich@gmail.com>
2025-02-05 21:08:47 +0000

    Add fuzzing
    Add 3 fuzzing entry points, each testing one of the 3 parsers we have:
    
      * `BuildDirUtil` (ninja build parser)
      * `DepsReader` (`.ninja_deps` parser)
      * `LogReader` (`.ninja_log` parser)
    
    In order to do this we break up `trimja` project into a `core` static
    library and the `trimja` executable.  This allows us to link the `core`
    library into the separate fuzzing executables.

---
+++ src/builddirutil.fuzz.cpp
@@ -0,0 +1,39 @@
+// MIT License
+//
+// Copyright (c) 2025 Elliot Goodrich
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "builddirutil.h"
+
+#include <sstream>
+#include <string>
+
+#include <stdint.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  using namespace trimja;
+  const std::string input{reinterpret_cast<const char*>(data), size};
+  try {
+    BuildDirUtil util;
+    util.builddir(std::filesystem::path{}, input);
+  } catch (const std::exception&) {
+  }
+  return 0;
+}
